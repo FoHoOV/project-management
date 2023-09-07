e6fdd1e4fefb4d46c098638de7ecfeef0ddf7e43
godzylinux <fohoov@gmail.com>
2023-09-08 00:24:05 +0330

    updated backend database name

--- backend/db/__init__.py
+++ backend/db/__init__.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
