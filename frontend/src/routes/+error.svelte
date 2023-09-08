14277c2f68c169bf0cec96095433245f7c479d72
godzylinux <fohoov@gmail.com>
2023-09-08 13:01:15 +0330

    added custom error page

---
+++ frontend/src/routes/+error.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { faWarning } from '@fortawesome/free-solid-svg-icons';
+	import Fa from 'svelte-fa/src/fa.svelte';
+</script>
+
+<div>
+    <h1 class="font-bold text-4xl mb-5">Whoops! an error has occurred!</h1>
+    <div class="flex flex-row items-center gap-2">
+        <Fa icon={faWarning} />
+        <p class="text-lg">
+            Error <span class="font-bold text-xl">{$page.status}</span>
+        </p>
+    </div>
+    <p class="text-lg">
+        Please try again! {$page.error?.message}
+    </p>
+</div>
