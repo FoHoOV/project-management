46f05a49461d92ae68c100d0a7d6a807a81bb614
godzylinux <fohoov@gmail.com>
2023-10-14 21:34:20 +0330

    now todolists dont get more space than the height of their parent to create vertical scroll of main element

--- frontend/src/lib/components/todo/TodoList.svelte
+++ frontend/src/lib/components/todo/TodoList.svelte
@@ -60,7 +60,7 @@
 </script>
 
 <div
-	class="relative flex h-full w-full flex-col items-center rounded-xl border border-success-content p-5 {className}"
+	class="relative flex max-h-full w-full flex-col items-center overflow-y-auto rounded-xl border border-success-content p-5 {className}"
 >
 	<Error message={apiErrorTitle} />
 	{#if isCallingService}
--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -39,7 +39,7 @@
 				class="loading loading-ball loading-lg absolute left-[50%] top-[50%] -translate-x-1/2 -translate-y-1/2"
 			/>
 		{:else}
-			<div class="mx-auto px-6">
+			<div class="mx-auto px-6 h-full">
 				<slot />
 			</div>
 		{/if}
