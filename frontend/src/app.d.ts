9052d4ed819efda73697a4fe07f45c95d8cb05c3
godzylinux <fohoov@gmail.com>
2023-11-18 21:11:27 +0330

    where the fuck was this comment?

--- frontend/src/app.d.ts
+++ frontend/src/app.d.ts
@@ -1,8 +1,8 @@
 // See https://kit.svelte.dev/docs/types#app
+// for information about these interfaces
 
 import type { Token } from '$lib/client';
 
-// for information about these interfaces
 declare global {
 	namespace App {
 		interface Error {
