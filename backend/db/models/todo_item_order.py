d463918a892e513ec0c1cee03dedebd4aa32f515
godzylinux <fohoov@gmail.com>
2023-12-07 00:37:03 +0330

    removed moving_id from todos table

--- backend/db/models/todo_item_order.py
+++ backend/db/models/todo_item_order.py
@@ -17,7 +17,6 @@ class TodoItemOrder(BasesWithCreatedDate, BaseOrderedItem):
     right_id: Mapped[int | None] = mapped_column(
         ForeignKey("todo_item.id"), nullable=True, unique=True
     )
-    moving_id: Mapped[int] = mapped_column(ForeignKey("todo_item.id"), unique=True)
     todo: Mapped["TodoItem"] = relationship(foreign_keys=[todo_id], single_parent=True, back_populates="order")  # type: ignore
 
     __table_args__ = (
