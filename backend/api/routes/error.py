4b704b46300595fec617b32617045c45b8d349d2
godzylinux <fohoov@gmail.com>
2023-12-12 19:43:36 +0330

    fixed error-types route?

--- backend/api/routes/error.py
+++ backend/api/routes/error.py
@@ -1,22 +1,21 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
 
-
-from error.exceptions import ErrorCode, UserFriendlyError
+from error.exceptions import ErrorCode
 
 
 router = APIRouter(prefix="/error", tags=["error"])
 
 
-class ErrorCodesSchema(BaseModel):
-    codes: list[ErrorCode]
+class UserFriendlyErrorSchema(BaseModel):
+    code: ErrorCode
+    description: str
 
 
 # a hack just to include these types in the generated OpenApi json
 @router.get(
     path="/error-types",
-    response_model=ErrorCodesSchema,
-    responses={400: {"error": UserFriendlyError}},
+    response_model=UserFriendlyErrorSchema,
 )
 def include_error_types():
     raise
