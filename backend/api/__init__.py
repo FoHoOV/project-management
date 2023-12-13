33c2d37d32f9b997c2b7c870bf7799232a452bc2
godzylinux <fohoov@gmail.com>
2023-12-13 22:06:04 +0330

    temporary solution to get a successful publish

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -41,7 +41,7 @@ def create_app():
 
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=origins,
+        allow_origins=["*"],  # TODO: origins
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
