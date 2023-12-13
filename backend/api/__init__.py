b9e86a9cc509b0c74ee53ca086e12c5ab40535f3
godzylinux <fohoov@gmail.com>
2023-12-13 22:15:33 +0330

    as it figures what im doing is right, the provider's config is wrong

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -41,7 +41,7 @@ def create_app():
 
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["*"],  # TODO: origins
+        allow_origins=origins,
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
