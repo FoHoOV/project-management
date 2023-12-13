8e660d374873a24a9c707668a992c87662f71a6c
Alireza GoDzY <stm.stm155@gmail.com>
2023-12-13 18:21:14 +0330

    updated run-windows.cmd to autogenerate .env file

--- run-windows.cmd
+++ run-windows.cmd
@@ -3,6 +3,11 @@ call git pull
 cd backend
 if not exist venv python -m venv venv
 call venv\Scripts\pip.exe install -r requirements.txt
+del .env
+rem Saved in .\.env
+@echo off
+echo ALLOWED_ORIGINS = ["http://localhost", "http://localhost:4173","http://localhost:5173", "http://localhost:5174"]> .\.env
+echo IS_LOG_SQLALCHEMY_ENABLED = True >> .\.env
 start cmd.exe @cmd /k "venv\Scripts\python -m uvicorn main:app --reload --port 8080"
 cd ..
 cd frontend
