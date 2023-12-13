e48eef6fca39dbfd3d077a8fc47e2a928d7c262a
godzylinux <fohoov@gmail.com>
2023-12-13 20:50:08 +0330

    auto generating openapijson to support multiple environments

---
+++ frontend/generate-openapitools-json.js
@@ -0,0 +1,25 @@
+import fs from 'fs';
+import { join } from 'path';
+
+const openapitoolsJson = {
+	$schema: './node_modules/@openapitools/openapi-generator-cli/config.schema.json',
+	spaces: 2,
+	'generator-cli': {
+		version: '7.0.0',
+		generators: {
+			'typescript-fetch': {
+				generatorName: 'typescript-fetch',
+				inputSpec: process.env.PUBLIC_API_URL,
+				output: './src/lib/generated-client',
+				additionalProperties: {
+					modelPropertyNaming: 'original',
+					supportsES6: true,
+					useSingleRequestParameter: false
+				}
+			}
+		}
+	}
+};
+
+const SCHEMA_FILE_PATH = join('./', 'openapitools.json');
+fs.writeFileSync(SCHEMA_FILE_PATH, JSON.stringify(openapitoolsJson, null, 2));
