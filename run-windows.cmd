22fa664f92056e3f99c6414dfc5d44dbb6f7f712
Alireza GoDzY <stm.stm155@gmail.com>
2023-12-13 21:10:35 +0330

    updated run-windows command to match newest updates

--- run-windows.cmd
+++ run-windows.cmd
@@ -6,8 +6,9 @@ call venv\Scripts\pip.exe install -r requirements.txt
 del .env
 rem Saved in .\.env
 @echo off
-echo ALLOWED_ORIGINS = ["http://localhost", "http://localhost:4173","http://localhost:5173", "http://localhost:5174"]> .\.env
+echo ALLOWED_ORIGINS = ["http://localhost", "http://localhost:4173","http://localhost:5173", "http://localhost:5174"] > .\.env
 echo IS_LOG_SQLALCHEMY_ENABLED = True >> .\.env
+echo SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db" >> .\.env
 start cmd.exe @cmd /k "venv\Scripts\python -m uvicorn main:app --reload --port 8080"
 cd ..
 cd frontend
@@ -16,7 +17,7 @@ rem Saved in .\.env
 @echo off
 echo PUBLIC_API_URL = http://127.0.0.1:8080 > .\.env
 call npm i
-call npm run open-api:generate:windows
+call npm run open-api:generate:windows:dev
 call npm run build
 call npm run preview
 pause
