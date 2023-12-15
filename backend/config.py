f1b7a84ed4c10e914c6fe754744f3436a3426524
godzylinux <fohoov@gmail.com>
2023-12-15 20:40:08 +0330

    made SECRET_KEY .env based!

--- backend/config.py
+++ backend/config.py
@@ -2,11 +2,11 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    # to get a string like this run:
-    # openssl rand -hex 32
-    SECRET_KEY: str = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 120
+
+    # these configurations read from the .env file that you have to create
+    SECRET_KEY: str
     SQLALCHEMY_DATABASE_URL: str
     ALLOWED_ORIGINS: list[str]
     IS_SQLALCHEMY_LOG_ENABLED: bool
