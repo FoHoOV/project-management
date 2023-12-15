29f947cbf49da1d540e2e96ed9ed1d8047dd1496
godzylinux <fohoov@gmail.com>
2023-12-15 20:43:42 +0330

    updated run-windows to put SECRET_KEY in the generated .env file

--- run-windows.cmd
+++ run-windows.cmd
@@ -9,6 +9,7 @@ rem Saved in .\.env
 echo ALLOWED_ORIGINS = ["http://localhost", "http://localhost:4173","http://localhost:5173", "http://localhost:5174"] > .\.env
 echo IS_SQLALCHEMY_LOG_ENABLED  = True >> .\.env
 echo SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db" >> .\.env
+echo SECRET_KEY = "933bd8fa378e6e90ed4841d81986906b8d3eaa72fcc4fc8b0c452b3139164126" >> .\.env
 start cmd.exe @cmd /k "venv\Scripts\python -m uvicorn main:app --reload --port 8080"
 cd ..
 cd frontend
