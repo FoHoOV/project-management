ff552faa94622bb903675521c08a9ea91bfafd3e
godzylinux <fohoov@gmail.com>
2023-12-16 21:48:49 +0330

    improved landing page design?

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -2,14 +2,24 @@
 	<title>home</title>
 </svelte:head>
 
-<div class="hero bg-base-200">
-	<div class="hero-content text-center">
-		<div class="max-w-md">
-			<h1 class="text-5xl font-bold">A simple todo app created with svelte!</h1>
-			<p class="py-6">
-				For years I've been creating web apps using vanilla js/css but what the fuck I was doing
-				with my life?
-			</p>
+<div
+	class="hero min-h-[50%] rounded-md border border-secondary border-opacity-30 bg-base-200 shadow-2xl"
+>
+	<div class="hero-content lg:w-full">
+		<div
+			class="flex max-w-full flex-col gap-20 lg:w-full lg:flex-row-reverse lg:justify-center lg:px-5"
+		>
+			<h1
+				class="font-bolder inline-block self-center bg-gradient-to-r from-secondary via-secondary-focus to-primary bg-clip-text text-8xl text-transparent"
+			>
+				FoHoOV
+			</h1>
+			<div>
+				<p class="max-w-md py-6 text-lg font-semibold">
+					A project management app that tries to have every thing ASANA has, but step by step :D
+				</p>
+				<a href="/user/projects" class="link-secondary btn btn-outline link">get started</a>
+			</div>
 		</div>
 	</div>
 </div>
