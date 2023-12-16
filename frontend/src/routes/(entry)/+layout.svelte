acdd1a472a3f37241b3cd67b1e3e56c1598865c7
godzylinux <fohoov@gmail.com>
2023-12-16 22:37:51 +0330

    improved account entry design

--- frontend/src/routes/(entry)/+layout.svelte
+++ frontend/src/routes/(entry)/+layout.svelte
@@ -1,12 +1,19 @@
-<div class="flex gap-5">
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<div class="flex items-center gap-5 rounded-2xl bg-base-200 shadow-md">
 	<div class="flex-1">
 		<slot />
 	</div>
-	<div class="hero bg-base-200 rounded-2xl p-y hidden lg:grid flex-1 shadow-md">
-		<div class="hero-content text-center">
-			<div>
-				<h1 class="text-5xl font-bold">Account management</h1>
-			</div>
-		</div>
+	<div class="hidden flex-1 md:block lg:text-center">
+		<h1 class="text-5xl font-bold text-info">Account management</h1>
+		<h2 class="pt-5 text-xl font-semibold">
+			{#if $page.url.href.endsWith('/login')}
+				Login to your existing account
+			{:else if $page.url.href.endsWith('/signup')}
+				Create a new account
+			{/if}
+		</h2>
 	</div>
 </div>
