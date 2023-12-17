d4150bb06decad3ecd7c12c872c7c620d7d566cb
godzylinux <fohoov@gmail.com>
2023-12-17 13:47:37 +0330

    improved home page design

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -10,7 +10,7 @@
 			class="flex max-w-full flex-col gap-20 lg:w-full lg:flex-row-reverse lg:justify-center lg:px-5"
 		>
 			<h1
-				class="font-bolder inline-block self-center bg-gradient-to-r from-secondary via-secondary-focus to-primary bg-clip-text text-8xl text-transparent"
+				class="font-bolder inline-block self-center bg-gradient-to-r from-secondary via-secondary-focus to-primary bg-clip-text text-7xl text-transparent sm:text-8xl"
 			>
 				FoHoOV
 			</h1>
