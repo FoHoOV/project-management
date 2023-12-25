df1f8608a38da02ed6f2a2dd805a4a0be0ae50f8
godzylinux <fohoov@gmail.com>
2023-12-25 21:44:28 +0330

    added the ability to connect to test server while testing integrations

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -2,7 +2,7 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
 	webServer: {
-		command: 'npm run build && npm run preview',
+		command: 'npm run build-integration && npm run preview',
 		port: 5174
 	},
 	testDir: 'tests',
