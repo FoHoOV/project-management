d201db4b117192d74c828465b7e11ddbc48acb5b
godzylinux <fohoov@gmail.com>
2023-12-25 20:23:40 +0330

    improved tests for entry page

--- frontend/tests/entry.test.ts
+++ frontend/tests/entry.test.ts
@@ -1,6 +1,6 @@
 import { expect, test } from '@playwright/test';
 
-test('login page contents', async ({ page }) => {
+test('checking login page contents', async ({ page }) => {
 	await page.goto('/login');
 
 	await expect(
@@ -15,4 +15,26 @@ test('login page contents', async ({ page }) => {
 		page.url().endsWith('/login'),
 		'page should still be on login page because inputs are empty'
 	).toBeTruthy();
+});
+
+test('changing entry page from login page', async ({ page }) => {
+	await page.goto('/login');
+
+	const signUpLink = await page.locator('form a[href="/signup"]');
+	await expect(signUpLink, 'redirect to signup page should exist in login page').toBeVisible();
+
+	await signUpLink.click();
+
+	await expect(page, 'page should be redirected to signup page').toHaveURL('signup');
+});
+
+test('changing entry page from signup page', async ({ page }) => {
+	await page.goto('/signup');
+
+	const loginLink = await page.locator('form a[href="/login"]');
+	await expect(loginLink, 'redirect to login page should exist in signup page').toBeVisible();
+
+	await loginLink.click();
+
+	await expect(page, 'page should be redirected to login page').toHaveURL('login');
 });
