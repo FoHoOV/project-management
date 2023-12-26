9b7e5a2e414f3615e5004fd2aeed272d7170c749
godzylinux <fohoov@gmail.com>
2023-12-26 11:16:56 +0330

    repeating login attempt 10 times with different capitalizations

--- backend/api/routes/oauth/test_oauth.py
+++ backend/api/routes/oauth/test_oauth.py
@@ -1,4 +1,6 @@
+from secrets import choice
 from fastapi.testclient import TestClient
+
 from api.test import TEST_USER, app
 
 
@@ -6,13 +8,19 @@ client = TestClient(app)
 
 
 def test_login():
-    response = client.post(
-        "/oauth/token",
-        headers={"Content-Type": "application/x-www-form-urlencoded"},
-        data={"username": TEST_USER["username"], "password": TEST_USER["password"]},
-    )
+    for _ in range(10):
+        response = client.post(
+            "/oauth/token",
+            headers={"Content-Type": "application/x-www-form-urlencoded"},
+            data={
+                "username": "".join(
+                    choice((str.upper, str.lower))(c) for c in TEST_USER["username"]
+                ),
+                "password": TEST_USER["password"],
+            },
+        )
 
-    assert response.status_code == 200
-    assert (
-        "access_token" in response.json()
-    ), "after a successful login we should get an access_token"
+        assert response.status_code == 200
+        assert (
+            "access_token" in response.json()
+        ), "after a successful login we should get an access_token"
