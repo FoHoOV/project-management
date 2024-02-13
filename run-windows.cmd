1c7b3ae78fa25f4dec2b7cc09dac7877108caec7
Alireza GoDzY <stm.stm155@gmail.com>
2024-02-14 00:47:13 +0330

    fix: updated run-windows to npm i with legacy-peer-deps flag on because some packages like svelte-fa hasn't updated their peerDep svelte version range

--- run-windows.cmd
+++ run-windows.cmd
@@ -41,7 +41,7 @@ del .env.integration
 echo PUBLIC_API_URL = http://127.0.0.1:8090 > .\.env.integration
 
 REM installing dependencies
-call npm i
+call npm i --legacy-peer-deps
 call npm run open-api:generate:windows
 call npm run build
 call npm run preview
