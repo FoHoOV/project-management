a8c3bbc4248a6ebaeb1b291c9f253a8efbc12341
godzylinux <fohoov@gmail.com>
2024-02-20 15:57:40 +0330

    made actions generic

--- frontend/src/lib/components/todos/todo-list/TodoListActions.svelte
+++ frontend/src/lib/components/todos/todo-list/TodoListActions.svelte
@@ -25,12 +25,13 @@
 
 	async function handleUpdateAction(event: Event) {
 		componentState = 'calling-service';
+		const cachedCheckedState = (event.target as HTMLInputElement).checked;
 		await callServiceInClient({
 			serviceCall: async () => {
 				const result = await TodoCategoryClient({ token: $page.data.token }).updateItemTodoCategory(
 					{
 						id: category.id,
-						actions: (event.target as HTMLInputElement).checked ? [Action.Done] : [Action.Undone]
+						actions: [(event.target as HTMLInputElement).name as Action]
 					}
 				);
 
@@ -41,21 +42,23 @@
 			errorCallback: async (e) => {
 				apiErrorTitle = e.message;
 				componentState = 'none';
-				(event.target as HTMLInputElement).checked = false;
+				(event.target as HTMLInputElement).checked = cachedCheckedState;
 			}
 		});
 	}
 </script>
 
-<div class="relative">
-	<Spinner visible={componentState === 'calling-service'}></Spinner>
-	<Alert type="error" message={apiErrorTitle} class="mb-2" />
-	<FormInput
-		label="Mark as done when dropped"
-		name="done-action"
-		inputClasses="checkbox-warning"
-		labelClasses="flex max-w-full cursor-pointer items-center justify-between gap-2 rounded-md border border-info p-3"
-		onchange={handleUpdateAction}
-		checked={category.actions.filter((action) => action.action == Action.Done).length > 0}
-	></FormInput>
-</div>
+<Alert type="error" message={apiErrorTitle} class="mb-2" />
+{#each Object.values(Action) as action}
+	<div class="relative">
+		<Spinner visible={componentState === 'calling-service'}></Spinner>
+		<FormInput
+			label="Mark as done when dropped"
+			name={action}
+			inputClasses="checkbox-warning"
+			labelClasses="flex max-w-full cursor-pointer items-center justify-between gap-2 rounded-md border border-info p-3"
+			onchange={handleUpdateAction}
+			checked={category.actions.some((value) => value.action == action)}
+		></FormInput>
+	</div>
+{/each}
