867f3eb98ec4ee21af5cf7446185a6984c06c8bc
m-setayesh-m <m-setayesh-m@agri-bank.com>
2024-02-21 09:48:21 +0330

    chore: added runtime validations for join_with_permission_query_if_required function to already be joined with project and prevent double joins with UserProjectPermission

--- backend/db/utils/shared/permission_query.py
+++ backend/db/utils/shared/permission_query.py
@@ -21,6 +21,18 @@ def join_with_permission_query_if_required[
     if all(permission != Permission.ALL for permission in permissions):
         permissions = permissions + [Permission.ALL]
 
+    # TODO: THERE SHOULD BE A BETTER WAY, but the purpose is that the query should already be joined with Projects table
+    # because if its not we are not sure whose permissions we are checking
+    if not any(joins[0].parent.entity == Project for joins in query._setup_joins):  # type: ignore
+        raise Exception("query should be already joined with Project table")
+
+    # TODO: THERE SHOULD BE A BETTER WAY, but the purpose is that the query should not already be joined with UserProjectPermission table
+    # otherwise we would be joining to UserProjectPermission table twice
+    if any(
+        joins[0].parent.entity == UserProjectPermission for joins in query._setup_joins  # type: ignore
+    ):
+        raise Exception("query is already joined with UserProjectPermission")
+
     query = query.join(ProjectUserAssociation.permissions).filter(
         UserProjectPermission.permission.in_(permissions)
     )
