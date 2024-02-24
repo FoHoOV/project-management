7ed8b618ec2bbc548d1215f4b3a39e8b053e3bd4
godzylinux <fohoov@gmail.com>
2024-02-24 22:46:45 +0330

    fix: fixed vitest+testing-lib with svelte5

--- frontend/src/lib/components/buttons/LoadingButton.svelte.spec.ts
+++ frontend/src/lib/components/buttons/LoadingButton.svelte.spec.ts
@@ -1,21 +1,20 @@
-// TODO: pending fix for svelte-5
-// import { render, screen } from '@testing-library/svelte';
-// import LoadingButton from './LoadingButton.svelte';
-// import { expect, test } from 'vitest';
+import { render, screen } from '@testing-library/svelte/svelte5';
+import LoadingButton from './LoadingButton.svelte';
+import { expect, test } from 'vitest';
 
-// test('has text when loading is false', async () => {
-// 	render(LoadingButton, { text: 'test', loading: false });
-// 	const button = screen.getByRole('button');
+test('has text when loading is false', async () => {
+	render(LoadingButton, { text: 'test', loading: false });
+	const button = screen.getByRole('button');
 
-// 	expect(button, 'expected to have button with text=test').toHaveTextContent('test');
-// });
+	expect(button, 'expected to have button with text=test').toHaveTextContent('test');
+});
 
-// test('has loading state when loading is true', async () => {
-// 	render(LoadingButton, { text: 'test', loading: true });
-// 	const button = screen.getByRole('button');
+test('has loading state when loading is true', async () => {
+	render(LoadingButton, { text: 'test', loading: true });
+	const button = screen.getByRole('button');
 
-// 	expect(
-// 		button.getElementsByClassName('loading').length == 1,
-// 		'expected to have loading state'
-// 	).toBeTruthy();
-// });
+	expect(
+		button.getElementsByClassName('loading').length == 1,
+		'expected to have loading state'
+	).toBeTruthy();
+});
--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -1,12 +1,14 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig, configDefaults } from 'vitest/config';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
-	plugins: [svelte({ hot: !process.env.VITEST })],
+	plugins: [svelte()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}'],
 		globals: true,
 		environment: 'jsdom',
-		setupFiles: 'vitest-setup'
+		setupFiles: 'vitest-setup',
+		// Exclude playwright tests folder
+		exclude: [...configDefaults.exclude, 'tests']
 	}
 });
