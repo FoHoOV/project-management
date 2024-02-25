5a8286e0fd9058158d8b9ff62a099c47f589f74d
Alireza GoDzY <stm.stm155@gmail.com>
2024-02-25 17:55:45 +0330

    added the test files that i need to implement

---
+++ frontend/tests/routes/user/[project_name]-[project_id=integer]/todo-comments.test.ts
@@ -0,0 +1,13 @@
+import { test } from '@playwright/test';
+
+test('create a comment', ({ page }) => {
+	// test the comment counter has increased
+});
+
+test('update a comment', ({ page }) => {
+	// test the comment counter is the same
+});
+
+test('delete a comment', ({ page }) => {
+	// test the comments counter has decreased
+});
---
+++ frontend/tests/routes/user/[project_name]-[project_id=integer]/todo-item.test.ts
@@ -0,0 +1,11 @@
+import { test } from '@playwright/test';
+
+test('creating todo items', ({ page }) => {});
+
+test('deleting todo items', ({ page }) => {});
+
+test('editing todo items', ({ page }) => {});
+
+test('moving todo to another category', ({ page }) => {});
+
+test('reorder todo items', ({ page }) => {});
