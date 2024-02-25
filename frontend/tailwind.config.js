d09f403381e566d5af46690eb614910a36ba0a65
Alireza GoDzY <stm.stm155@gmail.com>
2024-02-25 13:44:57 +0330

    fix: I forgot the actually call the typography xD

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,7 +6,7 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	darkMode: ['class', '[data-theme="dark"]'],
-	plugins: [typography, daisyui],
+	plugins: [typography(), daisyui],
 	daisyui: {
 		themes: ['light', 'dark']
 	},
