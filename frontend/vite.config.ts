f878fffa1697ee537cd5bba1c495f5bb26265ed4
Alireza GoDzY <stm.stm155@gmail.com>
2024-02-26 23:20:51 +0330

    removed whole test https stuff
    since it can passed as npm script parameters and doesnt need to be involved in the codebase

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,20 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
-import { readFileSync } from 'fs';
 import 'dotenv/config';
-
-let serverSettings = {};
-if (process.env.TEST_HTTPS == 'true') {
-	serverSettings = {
-		server: {
-			https: {
-				key: readFileSync(`${__dirname}/cert/key.pem`),
-				cert: readFileSync(`${__dirname}/cert/cert.pem`)
-			},
-			proxy: {}
-		}
-	};
-}
 
 export default defineConfig({
 	plugins: [sveltekit()],
@@ -22,6 +8,5 @@ export default defineConfig({
 		include: ['src/**/*.{test,spec}.{js,ts}'],
 		globals: true,
 		environment: 'jsdom'
-	},
-	...serverSettings
+	}
 });
--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -1,20 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig, configDefaults } from 'vitest/config';
-import { readFileSync } from 'fs';
 import 'dotenv/config';
-
-let serverSettings = {};
-if (process.env.TEST_HTTPS == 'true') {
-	serverSettings = {
-		server: {
-			https: {
-				key: readFileSync(`${__dirname}/cert/key.pem`),
-				cert: readFileSync(`${__dirname}/cert/cert.pem`)
-			},
-			proxy: {}
-		}
-	};
-}
 
 export default defineConfig({
 	plugins: [sveltekit()],
@@ -25,6 +11,5 @@ export default defineConfig({
 		setupFiles: 'vitest-setup',
 		// Exclude playwright tests folder
 		exclude: [...configDefaults.exclude, 'tests']
-	},
-	...serverSettings
+	}
 });
