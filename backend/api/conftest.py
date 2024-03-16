274e69443898ac60e1726b00c0eb1b182f901aa4
godzylinux <fohoov@gmail.com>
2024-03-16 19:16:15 +0330

    tests: improved test setup
    previously session-scoped fixtures didn't use the already existing fixtures, also the DB initialization order was not guaranteed, now it is

--- backend/api/conftest.py
+++ backend/api/conftest.py
@@ -1,77 +1,77 @@
 from typing import List, TypedDict
 
+
+from fastapi import FastAPI
 from fastapi.testclient import TestClient
 import pytest
 from api import create_app
 from api.dependencies.db import get_db
-from api.dependencies.db_test import get_test_db
-from db.schemas.user import UserCreate
-from db.test import init_db
 
-from db.utils.user_crud import create_user
+from db.schemas.user import User
+from db.test import init_db, SessionLocalTest
 
 
-TestUserType = TypedDict("TestUserType", {"username": str, "password": str})
+TestUserType = TypedDict("TestUserType", {"id": int, "username": str, "password": str})
 
 _TEST_USERS: List[TestUserType] = [
-    {"username": "test_username1", "password": "test_password1"},
-    {"username": "test_username2", "password": "test_password2"},
-    {"username": "test_username3", "password": "test_password3"},
+    {"id": -1, "username": "test_username1", "password": "test_password1"},
+    {"id": -1, "username": "test_username2", "password": "test_password2"},
+    {"id": -1, "username": "test_username3", "password": "test_password3"},
 ]
 
 
 @pytest.fixture(scope="session")
-def test_app():
-    """Create and return a test FastAPI application."""
-    app = create_app()
-    app.dependency_overrides[get_db] = get_test_db
-    return app
-
-
-@pytest.fixture(scope="session")
-def test_client(test_app):
+def test_client(test_app: FastAPI):
     """Create a TestClient using the test FastAPI application."""
     with TestClient(test_app) as client:
         yield client
 
 
 @pytest.fixture(scope="session")
-def test_db():
-    """Fixture to provide a database session for testing."""
+def test_app():
+    """Create and return a test FastAPI application."""
     init_db()  # Ensure the database is initialized
-    db = next(
-        get_test_db()
-    )  # Manually get the first (and only) yield which is the session object
-    try:
-        yield db
-    finally:
-        db.close()  # Ensure the session is closed after the test(s)
+    app = create_app()
+
+    def get_test_db():
+        db = SessionLocalTest()
+        try:
+            yield db
+        finally:
+            db.close()
+
+    app.dependency_overrides[get_db] = get_test_db
+    return app
 
 
 @pytest.fixture(scope="session", autouse=True)
-def test_users(test_db):
+def test_users(test_client: TestClient, test_app: FastAPI):
     """Create test users in the database, ensuring each user is created only once."""
     for user in _TEST_USERS:
-        create_user(
-            test_db,
-            UserCreate.model_validate(
-                {
-                    "username": user["username"],
-                    "password": user["password"],
-                    "confirm_password": user["password"],
-                }
-            ),
+        response = test_client.post(
+            "/user/signup",
+            json={
+                "username": user["username"],
+                "password": user["password"],
+                "confirm_password": user["password"],
+            },
         )
+
+        assert response.status_code == 200
+
+        parsed_user = User.model_validate(response.json(), strict=True)
+        user["id"] = parsed_user.id
+
     return _TEST_USERS
 
 
 @pytest.fixture(scope="function")
-def access_token_factory(test_app):
+def access_token_factory(test_client: TestClient):
 
     def _get_access_token(
         user: TestUserType,
     ) -> str:
-        response = TestClient(test_app).post(
+        response = test_client.post(
             "/oauth/token",
             headers={"Content-Type": "application/x-www-form-urlencoded"},
             data={
