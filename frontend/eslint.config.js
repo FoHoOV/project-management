2856bdbc6ddb50e7a59e64c90ff7fdf253fef369
godzylinux <fohoov@gmail.com>
2024-04-12 16:06:29 +0330

    used eslint js config instead of cjs

---
+++ frontend/eslint.config.js
@@ -0,0 +1,39 @@
+import js from '@eslint/js';
+import ts from 'typescript-eslint';
+import svelte from 'eslint-plugin-svelte';
+import prettier from 'eslint-config-prettier';
+import globals from 'globals';
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+export default [
+	js.configs.recommended,
+	...ts.configs.recommended,
+	...svelte.configs['flat/recommended'],
+	prettier,
+	...svelte.configs['flat/prettier'],
+	{
+		languageOptions: {
+			globals: {
+				...globals.browser,
+				...globals.node
+			}
+		}
+	},
+	{
+		rules: {
+			'@typescript-eslint/no-unused-vars': ['warn'],
+			'@typescript-eslint/no-floating-promises': 'warn'
+		}
+	},
+	{
+		files: ['**/*.svelte'],
+		languageOptions: {
+			parserOptions: {
+				parser: ts.parser
+			}
+		}
+	},
+	{
+		ignores: ['build/', '.svelte-kit/', 'package/']
+	}
+];
