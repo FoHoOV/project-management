5123c60785de60902499b47a58ca345f9730da85
FoHoOV <53280503+FoHoOV@users.noreply.github.com>
2024-08-01 15:45:26 +0330

    tests: refactored todo comment tests to use fixtures and separated it into multiple tests

---
+++ backend/tests/api/fixtures/todo_comments.py
@@ -0,0 +1,59 @@
+from typing import Callable
+from fastapi.testclient import TestClient
+from httpx import Response
+import pytest
+from db.schemas.todo_item_comment import TodoComment
+from tests.api.conftest import TestUserType
+
+
+@pytest.fixture(scope="function")
+def create_comment_request(
+    test_client: TestClient,
+    auth_header_factory: Callable[[TestUserType], dict[str, str]],
+):
+    def _create_comment(user: TestUserType, todo_item_id: int, message: str):
+        response = test_client.post(
+            f"/todo-items/{todo_item_id}/comments",
+            headers=auth_header_factory(user),
+            json={"message": message},
+        )
+        return response
+
+    return _create_comment
+
+
+@pytest.fixture(scope="function")
+def create_comment(
+    create_comment_request: Callable[[TestUserType, int, str], Response],
+):
+    def _create_comment(user: TestUserType, todo_item_id: int, message: str):
+        response = create_comment_request(user, todo_item_id, message)
+        return TodoComment.model_validate(response.json(), strict=True)
+
+    return _create_comment
+
+
+@pytest.fixture(scope="function")
+def delete_comment_request(
+    test_client: TestClient,
+    auth_header_factory: Callable[[TestUserType], dict[str, str]],
+):
+    def _delete_comment(user: TestUserType, todo_item_id: int, comment_id: int):
+        response = test_client.delete(
+            f"/todo-items/{todo_item_id}/comments/{comment_id}",
+            headers=auth_header_factory(user),
+        )
+        return response
+
+    return _delete_comment
+
+
+@pytest.fixture(scope="function")
+def delete_comment(
+    delete_comment_request: Callable[[TestUserType, int, int], Response],
+):
+    def _delete_comment(user: TestUserType, todo_item_id: int, comment_id: int):
+        response = delete_comment_request(user, todo_item_id, comment_id)
+        return response.status_code
+
+    return _delete_comment
