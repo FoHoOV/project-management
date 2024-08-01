8f1b4112777925c88d876154621dad56f60effba
FoHoOV <53280503+FoHoOV@users.noreply.github.com>
2024-08-01 13:15:01 +0330

    tests: added verify_user_permissions fixture

--- backend/tests/api/fixtures/permissions.py
+++ backend/tests/api/fixtures/permissions.py
@@ -4,6 +4,7 @@ import pytest
 
 from db.models.user_project_permission import Permission
 
+from db.schemas.project import Project
 from tests.api.conftest import TestUserType
 
 
@@ -12,20 +13,40 @@ def update_user_permissions(
     test_client: TestClient,
     auth_header_factory: Callable[[TestUserType], dict[str, str]],
 ):
+
     def _update_permissions(
-        user: TestUserType,
+        owner: TestUserType,
+        target: TestUserType,
         project_id: int,
-        target_user_id: int,
         permissions: list[Permission],
     ):
         response = test_client.put(
             f"/permissions/{project_id}",
-            headers=auth_header_factory(user),
+            headers=auth_header_factory(owner),
             json={
-                "user_id": target_user_id,
+                "user_id": target["id"],
                 "permissions": permissions,
             },
         )
         return response
 
     return _update_permissions
+
+
+@pytest.fixture(scope="function")
+def verify_user_permissions(search_project: Callable[[TestUserType, int], Project]):
+    def _verify_permissions(
+        user: TestUserType,
+        project: Project,
+        expected_permissions: dict[int, list[Permission]],
+    ):
+        project_details = search_project(user, project.id)
+        for id, permissions in expected_permissions.items():
+            user_permissions = next(
+                user_data for user_data in project_details.users if user_data.id == id
+            ).permissions
+            assert sorted(user_permissions) == sorted(
+                permissions
+            ), f"Permissions for user_id {id} do not match"
+
+    return _verify_permissions
--- backend/tests/api/routes/permissions/test_permission.py
+++ backend/tests/api/routes/permissions/test_permission.py
@@ -1,3 +1,4 @@
+from encodings import search_function
 from typing import Callable
 
 from httpx import Response
@@ -21,6 +22,9 @@ def test_permissions_dont_leak(
         [TestUserType, TestUserType, int, list[Permission]], None
     ],
     search_project: Callable[[TestUserType, int], Project],
+    verify_user_permissions: Callable[
+        [TestUserType, Project, dict[int, list[Permission]]], None
+    ],
     test_users: list[TestUserType],
 ):
     user_a = test_users[0]  # Owner
@@ -31,20 +35,19 @@ def test_permissions_dont_leak(
     project_one = create_project(user_a)
     project_two = create_project(user_b)
 
+    # Attach users to projects with specific permissions
     attach_project_to_user(
         user_a,
         user_b,
         project_one.id,
         [Permission.CREATE_COMMENT, Permission.CREATE_TODO_CATEGORY],
     )
-
     attach_project_to_user(
         user_b,
         user_a,
         project_two.id,
         [Permission.CREATE_TODO_ITEM, Permission.CREATE_TODO_CATEGORY],
     )
-
     attach_project_to_user(
         user_b,
         user_c,
@@ -52,64 +55,63 @@ def test_permissions_dont_leak(
         [Permission.CREATE_TODO_ITEM, Permission.CREATE_TODO_CATEGORY],
     )
 
-    project_a = search_project(user_b, project_one.id)
-
-    assert len(project_a.users) == 2
-
-    user_a_permissions = next(
-        filter(lambda user: user.id == user_a["id"], project_a.users)
-    )
-    user_b_permissions = next(
-        filter(lambda user: user.id == user_b["id"], project_a.users)
-    )
-
-    assert user_a_permissions.permissions.sort() == [Permission.ALL].sort()
-    assert (
-        user_b_permissions.permissions.sort()
-        == [Permission.CREATE_COMMENT, Permission.CREATE_TODO_CATEGORY].sort()
+    # Verify permissions in Project One
+    verify_user_permissions(
+        user_a,
+        search_project(user_a, project_one.id),
+        {
+            user_a["id"]: [Permission.ALL],
+            user_b["id"]: [
+                Permission.CREATE_COMMENT,
+                Permission.CREATE_TODO_CATEGORY,
+            ],
+        },
     )
 
-    project_b = search_project(user_b, project_two.id)
-
-    assert len(project_b.users) == 3
-
-    user_a_permissions = next(
-        filter(lambda user: user.id == user_a["id"], project_b.users)
-    )
-    user_b_permissions = next(
-        filter(lambda user: user.id == user_b["id"], project_b.users)
-    )
-
-    assert user_b_permissions.permissions.sort() == [Permission.ALL].sort()
-    assert (
-        user_a_permissions.permissions.sort()
-        == [Permission.CREATE_TODO_ITEM, Permission.CREATE_TODO_CATEGORY].sort()
+    # Verify permissions in Project Two
+    verify_user_permissions(
+        user_b,
+        search_project(user_a, project_two.id),
+        {
+            user_a["id"]: [
+                Permission.CREATE_TODO_ITEM,
+                Permission.CREATE_TODO_CATEGORY,
+            ],
+            user_b["id"]: [Permission.ALL],
+            user_c["id"]: [
+                Permission.CREATE_TODO_ITEM,
+                Permission.CREATE_TODO_CATEGORY,
+            ],
+        },
     )
 
 
 def test_prevent_user_without_access_from_updating_permissions(
     create_project: Callable[[TestUserType], Project],
     update_user_permissions: Callable[
-        [TestUserType, int, int, list[Permission]], Response
+        [TestUserType, TestUserType, int, list[Permission]], Response
     ],
     attach_project_to_user: Callable[
         [TestUserType, TestUserType, int, list[Permission]], None
+    ],
+    search_project: Callable[[TestUserType, int], Project],
+    verify_user_permissions: Callable[
+        [TestUserType, Project, dict[int, list[Permission]]], None
     ],
     test_users: list[TestUserType],
 ):
     user_a = test_users[0]  # Owner
     user_b = test_users[1]  # Shared user with permission
 
-    # Create projects
+    # Create a project and share it
     project_one = create_project(user_a)
-
     attach_project_to_user(
         user_a, user_b, project_one.id, [Permission.UPDATE_TODO_CATEGORY]
     )
 
     # User C (no access) tries to update permissions
     user_no_access_response = update_user_permissions(
-        user_a, project_one.id, test_users[2]["id"], [Permission.ALL]
+        user_a, test_users[2], project_one.id, [Permission.ALL]
     )
     assert user_no_access_response.status_code == 400
     assert (
@@ -117,74 +119,101 @@ def test_prevent_user_without_access_from_updating_permissions(
         == ErrorCode.USER_DOESNT_HAVE_ACCESS_TO_PROJECT
     )
 
+    # Verify that no permissions were changed
+    verify_user_permissions(
+        user_a,
+        search_project(user_a, project_one.id),
+        {
+            user_a["id"]: [Permission.ALL],
+            user_b["id"]: [Permission.UPDATE_TODO_CATEGORY],
+        },
+    )
+
 
 def test_owner_can_update_user_permissions(
     create_project: Callable[[TestUserType], Project],
     update_user_permissions: Callable[
-        [TestUserType, int, int, list[Permission]], Response
+        [TestUserType, TestUserType, int, list[Permission]], Response
     ],
     attach_project_to_user: Callable[
         [TestUserType, TestUserType, int, list[Permission]], None
     ],
+    verify_user_permissions: Callable[
+        [TestUserType, Project, dict[int, list[Permission]]], None
+    ],
+    search_project: Callable[[TestUserType, int], Project],
     test_users: list[TestUserType],
 ):
     user_a = test_users[0]  # Owner
     user_b = test_users[1]  # Shared user with permission
 
-    # Create projects
+    # Create a project and share it
     project_one = create_project(user_a)
-
     attach_project_to_user(
         user_a, user_b, project_one.id, [Permission.UPDATE_TODO_CATEGORY]
     )
 
     # Owner updates user_b permissions
     change_user_b_permissions = update_user_permissions(
-        user_a, project_one.id, user_b["id"], [Permission.ALL]
+        user_a, user_b, project_one.id, [Permission.ALL]
     )
     assert change_user_b_permissions.status_code == 200
     assert PartialUserWithPermission.model_validate(
         change_user_b_permissions.json()
     ).permissions == [Permission.ALL]
 
+    # Verify the updated permissions
+    verify_user_permissions(
+        user_a,
+        search_project(user_b, project_one.id),
+        {
+            user_a["id"]: [Permission.ALL],
+            user_b["id"]: [Permission.ALL],
+        },
+    )
+
 
 def test_shared_user_cant_update_owner_permissions(
     create_project: Callable[[TestUserType], Project],
     update_user_permissions: Callable[
-        [TestUserType, int, int, list[Permission]], Response
+        [TestUserType, TestUserType, int, list[Permission]], Response
     ],
     attach_project_to_user: Callable[
         [TestUserType, TestUserType, int, list[Permission]], None
+    ],
+    search_project: Callable[[TestUserType, int], Project],
+    verify_user_permissions: Callable[
+        [TestUserType, Project, dict[int, list[Permission]]], None
     ],
     test_users: list[TestUserType],
 ):
     user_a = test_users[0]  # Owner
     user_b = test_users[1]  # Shared user with permission
 
-    # Create projects
+    # Create a project and share it
     project_one = create_project(user_a)
-
-    project_two = create_project(user_b)
-
     attach_project_to_user(
         user_a, user_b, project_one.id, [Permission.UPDATE_TODO_CATEGORY]
     )
 
-    attach_project_to_user(
-        user_b,
-        user_a,
-        project_two.id,
-        [Permission.DELETE_COMMENT, Permission.DELETE_TODO_ITEM],
-    )
-
-    # Shared user (user_b) updates owner (user_a) permissions
+    # Shared user (user_b) tries to update owner (user_a) permissions
     change_user_a_permissions = update_user_permissions(
         user_b,
+        user_a,
         project_one.id,
-        user_a["id"],
         [Permission.DELETE_COMMENT, Permission.DELETE_TODO_ITEM],
     )
     assert change_user_a_permissions.status_code == 400
+
+    # Verify that permissions were not changed
+    verify_user_permissions(
+        user_a,
+        search_project(user_b, project_one.id),
+        {
+            user_a["id"]: [Permission.ALL],
+            user_b["id"]: [Permission.UPDATE_TODO_CATEGORY],
+        },
+    )
 
 
 def test_users_of_project_grows_after_share(
@@ -224,7 +253,7 @@ def test_users_of_project_grows_after_share(
 def test_changing_project_owner(
     create_project: Callable[[TestUserType], Project],
     update_user_permissions: Callable[
-        [TestUserType, int, int, list[Permission]], Response
+        [TestUserType, TestUserType, int, list[Permission]], Response
     ],
     attach_project_to_user: Callable[
         [TestUserType, TestUserType, int, list[Permission]], None
@@ -240,9 +269,7 @@ def test_changing_project_owner(
 
     attach_project_to_user(user_a, user_b, project_one.id, [Permission.ALL])
 
-    update_user_permissions(
-        user_b, project_one.id, user_a["id"], [Permission.CREATE_COMMENT]
-    )
+    update_user_permissions(user_b, user_a, project_one.id, [Permission.CREATE_COMMENT])
 
     searched_project = search_project(user_b, project_one.id)
 
