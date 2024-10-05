4509453c30192b0ba731fa243a022f99b6f4a916
FoHoOV <53280503+FoHoOV@users.noreply.github.com>
2024-10-06 01:44:14 +0330

    chore: fix some linting issues

--- frontend/src/lib/components/popups/MultiStepModal.svelte
+++ frontend/src/lib/components/popups/MultiStepModal.svelte
@@ -1,42 +1,43 @@
 <script lang="ts" module>
-import Modal from '$components/popups/Modal.svelte';
-import { getMultiStepModal } from '$lib/stores';
-import type { ComponentExports } from '$lib/utils/types/svelte';
+	import Modal from '$components/popups/Modal.svelte';
+	import { getMultiStepModal } from '$lib/stores';
+	import type { ComponentExports } from '$lib/utils/types/svelte';
 
-export type Props = {
-	class?: string;
-	closeButtonText?: string;
-};
+	export type Props = {
+		class?: string;
+	};
 </script>
 
 <script lang="ts">
-const { class: className = '', closeButtonText = 'close' }: Props = $props();
+	const { class: className = '' }: Props = $props();
 
-const multiStepModal = getMultiStepModal();
-let modal = $state<ComponentExports<typeof Modal> | null>(null);
-const currentStep = $derived(
-	multiStepModal.steps$.length > 0 ? multiStepModal.steps$[multiStepModal.steps$.length - 1] : null
-);
-const componentProps = $derived(currentStep?.props());
-const showGoBackButton = $derived(multiStepModal.steps$.length > 1);
+	const multiStepModal = getMultiStepModal();
+	let modal = $state<ComponentExports<typeof Modal> | null>(null);
+	const currentStep = $derived(
+		multiStepModal.steps$.length > 0
+			? multiStepModal.steps$[multiStepModal.steps$.length - 1]
+			: null
+	);
+	const componentProps = $derived(currentStep?.props());
+	const showGoBackButton = $derived(multiStepModal.steps$.length > 1);
 
-function handleClose() {
-	multiStepModal.clear();
-}
-
-function handleGoBack() {
-	multiStepModal.pop();
-}
-
-$effect(() => {
-	if (multiStepModal.showing$) {
-		modal?.show();
+	function handleClose() {
+		multiStepModal.clear();
 	}
-});
+
+	function handleGoBack() {
+		multiStepModal.pop();
+	}
+
+	$effect(() => {
+		if (multiStepModal.showing$) {
+			modal?.show();
+		}
+	});
 </script>
 
 <Modal title={currentStep?.title} class={className} onClosed={handleClose} bind:this={modal}>
-	{#snippet body({ show, close })}
+	{#snippet body()}
 		{#if currentStep?.component}
 			{@const Component = currentStep?.component}
 			<Component {...componentProps}></Component>
